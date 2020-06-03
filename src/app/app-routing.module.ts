c471d82f6201837e97c102d271a2cba63917f887
byrtus <byrtus@wp.pl>
2020-06-03 14:45:58 +0200

    create package customer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,26 +5,26 @@ import {AuthGuard} from "./providers/auth.guard";
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./pages/customer/tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'promo',
-    loadChildren: () => import('./pages/promo/promo.module').then( m => m.PromoPageModule),
+    loadChildren: () => import('./pages/customer/promo/promo.module').then(m => m.PromoPageModule),
     canActivate: [AuthGuard]
   },
   {
     path: 'wallet',
-    loadChildren: () => import('./pages/wallet/wallet.module').then( m => m.WalletPageModule),
+    loadChildren: () => import('./pages/customer/wallet/wallet.module').then(m => m.WalletPageModule),
     canActivate: [AuthGuard]
   },
   {
     path: 'qr',
-    loadChildren: () => import('./pages/qr/qr.module').then( m => m.QrPageModule),
+    loadChildren: () => import('./pages/customer/qr/qr.module').then(m => m.QrPageModule),
     canActivate: [AuthGuard]
   },
   {
     path: 'map',
-    loadChildren: () => import('./pages/map/map.module').then( m => m.MapPageModule),
+    loadChildren: () => import('./pages/customer/map/map.module').then(m => m.MapPageModule),
     canActivate: [AuthGuard]
   }
 ];
