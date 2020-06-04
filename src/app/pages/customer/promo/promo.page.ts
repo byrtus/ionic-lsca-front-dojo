186d96962e01c9c7526b67f2f3afd1a4e999acde
byrtus <byrtus@wp.pl>
2020-06-04 20:24:47 +0200

    add companies to promotion tab

--- src/app/pages/customer/promo/promo.page.ts
+++ src/app/pages/customer/promo/promo.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Config} from "@ionic/angular";
+import {AllCompaniesService} from "../../../providers/allCompanies.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-promo',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromoPage implements OnInit {
 
-  constructor() { }
+  companies: any[];
+
+  constructor(
+      public config: Config,
+      public companiesService: AllCompaniesService,
+      private http: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.getCompanies();
   }
 
+  getCompanies() {
+    this.companiesService.getCompanies().subscribe(
+        response => this.companies = response
+    );
+  }
 }
