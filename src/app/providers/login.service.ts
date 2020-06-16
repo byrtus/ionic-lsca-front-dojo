58453cde994ce264ce6cc8bdf0817cc1d16ec422
byrtus <byrtus@wp.pl>
2020-06-16 12:17:25 +0200

    add error messages and success messages for --> Login, customerRegistry and managerRegistry

--- src/app/providers/login.service.ts
+++ src/app/providers/login.service.ts
@@ -5,6 +5,7 @@ import {NavigationStart, Router, RouterEvent} from "@angular/router";
 import {environment} from "../../environments/environment";
 import {JwtHelperService} from "@auth0/angular-jwt";
 import {filter, map, tap} from "rxjs/operators";
+import {ToastController} from "@ionic/angular";
 
 
 @Injectable({
@@ -17,6 +18,7 @@ export class LoginService {
 
     constructor(private http: HttpClient,
                 private router: Router,
+                private toastCtrl: ToastController,
                 public jwtHelper: JwtHelperService) {
         router.events
             .pipe(
@@ -33,15 +35,29 @@ export class LoginService {
         return this.http.post<any>(`${environment.apiUrl}/login`, {username: username, password: password}, {observe: "response"})
     }
 
-    login(username: string, password: string) {
+    login(username: string, password: string): any {
         //jak coś może być trzeba obciąć "Bearer "??????
         this.postLogin(username, password)
-            .subscribe((response) => {
+            .subscribe(async response => {
                 console.log('test');
                 localStorage.setItem("Token", response.body['Authorization']);
                 this._userId = response.body['UserId'];
                 this.authenticationState$.next(this.isTokenValid());
+                const toast = await this.toastCtrl.create({
+                    position: 'top',
+                    duration: 3000,
+                    header: 'Login Successful',
+                    message: 'Welcome ' + username
+                });
+                await toast.present();
                 this.router.navigateByUrl('/tabs/wallet');
+            },async error => {
+                const toast = await this.toastCtrl.create({
+                    duration: 3000,
+                    header: 'Login Fail.',
+                    message: 'Bad login or password.'
+                });
+                await toast.present();
             });
     }
 
--- src/app/providers/register.service.ts
+++ src/app/providers/register.service.ts
@@ -1,58 +1,120 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
 import {JwtHelperService} from "@auth0/angular-jwt";
+import {ToastController} from "@ionic/angular";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor(private http: HttpClient,
-              private router: Router,
-              public jwtHelper: JwtHelperService
-  ) {
-  }
+    constructor(private http: HttpClient,
+                private router: Router,
+                private toastCtrl: ToastController,
+                public jwtHelper: JwtHelperService
+    ) {
+    }
 
-  postManager(username: string, password: string, firstName: string, lastName: string, email: String, companyName: string, city: string, zipCode: string, street: string, localNumber: string): Observable<any> {
-    return this.http.post<any>(`${environment.apiUrl}/api/register/manager`, {
-      username: username,
-      password: password,
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      companyName: companyName,
-      city: city,
-      zipCode: zipCode,
-      street: street,
-      localNumber: localNumber
-    }, {observe: "response"})
-  }
+    postManager(username: string, password: string, firstName: string, lastName: string, email: String, companyName: string, city: string, zipCode: string, street: string, localNumber: string): Observable<any> {
+        return this.http.post<any>(`${environment.apiUrl}/api/register/manager`, {
+            username: username,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            companyName: companyName,
+            city: city,
+            zipCode: zipCode,
+            street: street,
+            localNumber: localNumber
+        }, {observe: "response"})
+    }
 
-  postCustomer(username: string, password: string, firstName: string, lastName: string, email: String): Observable<any> {
-    return this.http.post<any>(`${environment.apiUrl}/api/register/user`, {
-      username: username,
-      password: password,
-      firstName: firstName,
-      lastName: lastName,
-      email: email
-    }, {observe: "response"})
-  }
+    postCustomer(username: string, password: string, firstName: string, lastName: string, email: String): Observable<any> {
+        return this.http.post<any>(`${environment.apiUrl}/api/register/user`, {
+            username: username,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            email: email
+        }, {observe: "response"})
+    }
 
-  managerRegister(username: string, password: string, firstName: string, lastName: string, email: string, companyName: string, city: string, zipCode: string, street: string, localNumber: string) {
-    this.postManager(username, password, firstName, lastName, email, companyName, city, zipCode, street, localNumber)
-        .subscribe((response) => {
-          console.log(response);
-        })
-  }
+    async managerRegister(username: string, password: string, firstName: string, lastName: string, email: string, companyName: string, city: string, zipCode: string, street: string, localNumber: string) {
+        if (this.managerRegistryVerification(username, password, firstName, lastName, email, companyName, city, zipCode, street, localNumber)){
+            this.postManager(username, password, firstName, lastName, email, companyName, city, zipCode, street, localNumber)
+                .subscribe(async response => {
+                    console.log(response);
+                    const toast = await this.toastCtrl.create({
+                        duration: 3000,
+                        header: 'Company Registration: Successful',
+                        message: 'Please Login'
+                    });
+                    await toast.present();
+                    this.router.navigateByUrl('/login');
+                }, async error => {
+                    const toast = await this.toastCtrl.create({
+                        duration: 3000,
+                        header: 'Company Registration: Fail',
+                        message: 'Can not Access to server'
+                    });
+                    await toast.present();
+                });
+        }else {
+            await (await this.toastCtrl.create({
+                message: 'All Data must be fill',
+                duration: 2000
+            })).present();
+        }
+    }
 
-  customerRegister(username: string, password: string, firstName: string, lastName: string, email: string) {
-    this.postCustomer(username, password, firstName, lastName, email)
-        .subscribe((response) => {
-          console.log(response);
-        })
-  }
+    async customerRegister(username: string, password: string, firstName: string, lastName: string, email: string) {
+        if (this.customerRegistryVerification(username, password, firstName, lastName, email)) {
+            this.postCustomer(username, password, firstName, lastName, email)
+                .subscribe(async response => {
+                    console.log(response);
+                    const toast = await this.toastCtrl.create({
+                        duration: 3000,
+                        header: 'User Registration: Successful',
+                        message: 'Please Login'
+                    });
+                    await toast.present();
+                    this.router.navigateByUrl('/login');
+                }, async error => {
+                    const toast = await this.toastCtrl.create({
+                        duration: 3000,
+                        header: 'User Registration: Fail',
+                        message: 'Can not Access to server'
+                    });
+                    await toast.present();
+                });
+        } else {
+            await (await this.toastCtrl.create({
+                message: 'All Data must be fill',
+                duration: 2000
+            })).present();
+        }
+    }
+
+    customerRegistryVerification(username: string, password: string, firstName: string, lastName: string, email: string): boolean {
+
+        if (username == '' || password == '' || firstName == '' || lastName == '' || email == '') {
+            return false
+        } else {
+            return true
+        }
+    }
+
+    managerRegistryVerification(username: string, password: string, firstName: string, lastName: string, email: string, companyName: string, city: string, zipCode: string, street: string, localNumber: string): boolean {
+
+        if (username == '' || password == '' || firstName == '' || lastName == '' || email == '' || companyName == '' || city == '' || zipCode == '' || street == '' || localNumber == '') {
+            return false
+        } else {
+            return true
+        }
+    }
 }
 
